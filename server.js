cb74a034b5ea7eb4d97a173d876a3869b5857e78
Jehyun <congmul@hotmail.com>
2020-12-04 01:30:23 -0800

    modified index.handlebars

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
