c4b76f56bfebee75aa7eb375931a988c326b82a4
Jehyun <congmul@hotmail.com>
2020-12-04 02:35:55 -0800

    Design page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,6 +6,7 @@ const burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
     burger.selectAll((data) => {
+        console.log(data);
         res.render("index", {burgers: data});
     });
 });
