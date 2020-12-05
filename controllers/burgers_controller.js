e4fa7c0093ae3cb9375b2fae23883238028ca1b1
Jehyun <congmul@hotmail.com>
2020-12-04 18:33:32 -0800

    Modified style

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,14 +6,12 @@ const burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
     burger.selectAll((data) => {
-        console.log(data);
         res.render("index", {burgers: data});
     });
 });
 
 
 router.post("/api/create", (req, res) => {
-    console.log("POST TEST");
     burger.insertOne(req.body.burger_name,(result) => {
         res.json( {id : result.insertId });
     });
