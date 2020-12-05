0329eea9120211869f7ed04d03b1ff426934291a
Jehyun <congmul@hotmail.com>
2020-12-04 23:07:23 -0800

    Modified data type const -> var

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 require("dotenv").config();
-const connection;
+var connection;
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
